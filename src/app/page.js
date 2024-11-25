a2cd4cfcbf99217d34b76f968db38f5be68044dd
Shahoriar Azad Niloy <niloyshahoriar@gmail.com>
2024-11-26 03:00:02 +0600

    Top Destinations Section Created

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import TopDestinations from "@/components/TopDestinations";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
       <Navbar></Navbar>
-      <Hero></Hero>       
+      <Hero></Hero>   
+      <TopDestinations></TopDestinations>    
       
     </div>
   );
