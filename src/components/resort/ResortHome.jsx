bbff0130d6572cc3345ed2f50a37354f6e9477ff
deveshbis <deveshbiswas.cse@gmail.com>
2024-11-27 17:22:19 +0600

    added premium service section

--- src/components/resort/ResortHome.jsx
+++ src/components/resort/ResortHome.jsx
@@ -3,6 +3,8 @@ import Banner from './Banner';
 import Room from './Room';
 import Facilities from './Facilities';
 import PhotoGallery from './PhotoGallery';
+import Testimonial from './Testimonial';
+import Service from './Service';
 
 const ResortHome = () => {
     return (
@@ -10,7 +12,9 @@ const ResortHome = () => {
             <Banner/>
             <Room />
             <Facilities/>
+            <Service/>
             <PhotoGallery/>
+            <Testimonial/>
         </div>
     );
 };
--- src/components/resort/TitleSection.jsx
+++ src/components/resort/TitleSection.jsx
@@ -5,7 +5,7 @@ const TitleSection = ({ subHeader, mainHeader }) => {
         <div>
             <div className="text-center mb-8 px-4">
                 <h2 className="text-3xl font-extrabold text-gray-800 inline-block relative after:absolute after:w-4/6 after:h-1 after:left-0 after:right-0 after:-bottom-4 after:mx-auto after:bg-blue-700 after:rounded-lg-full">{mainHeader}</h2>
-                <p className="mt-4 text-lg text-black font-thin">
+                <p className="mt-8 text-lg text-black font-thin">
                     {subHeader}
                 </p>
             </div>
