952fa447e14ec73351e66a405fed05c74ffb213a
asshikhon <asshikhon@gmail.com>
2024-11-28 23:23:05 +0600

    added new dashboard section AddPackages with post api method

---
+++ src/app/(dashboard)/dashboard/addPackages/api/route.js
@@ -0,0 +1,20 @@
+import { connectDB } from "@/lib/connectDB";
+import { NextResponse } from "next/server";
+
+export const POST = async (req) => {
+  const db = await connectDB();
+  const packagesCollection = await db.collection("packages");
+
+  try {
+    const body = await req.json(); 
+    const result = await packagesCollection.insertOne(body);
+
+    return NextResponse.json(
+      { message: "Package Added Successfully!", result },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error adding spot:", error);
+    return NextResponse.json({ error: "Failed to add the spot." }, { status: 500 });
+  }
+};
