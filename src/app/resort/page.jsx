a20839b3769e85b1f1656d197d4961f19ff3a35f
Shahoriar Azad Niloy <niloyshahoriar@gmail.com>
2024-11-30 23:36:22 +0600

    Fixed some error

--- src/app/resort/page.jsx
+++ src/app/resort/page.jsx
@@ -1,7 +1,7 @@
 
 "use client";
 import React, { Suspense } from "react";
-import SingleResort from "./SingleResort";
+import SingleResort from "../singleResort/page";
 
 const ResortPage = () => {
     return (
