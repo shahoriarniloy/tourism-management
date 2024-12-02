96bb4b3f2c00e3cd0f56b2b2f9a3c5bdb4de8872
Shahoriar Azad Niloy <niloyshahoriar@gmail.com>
2024-12-03 02:14:18 +0600

    UI Modified

--- src/components/destinations/DestinationBody.jsx
+++ src/components/destinations/DestinationBody.jsx
@@ -59,14 +59,14 @@ if (error) return <p>Error: {error}</p>;
           >
             <figure className="w-full h-full rounded-md overflow-hidden">
               <Image
-                src={destination.photoURL1}
+                src={destination.photoURL2}
                 alt={destination.name}
                 width={600}
                 height={600}
                 className="h-full w-full scale-105 group-hover:scale-100 rounded-lg object-cover transition-all duration-300"
               />
             </figure>
-            <div className="absolute top-0 left-0 w-full h-full transition-all duration-300 bg-gradient-to-b from-[#2b2f3325] via-[#2832365b] to-[#0a52be] opacity-45"></div>
+            <div className="absolute top-0 left-0 w-full h-full transition-all duration-300  "></div>
             <article className="p-4 space-y-2 absolute -bottom-10 group-hover:bottom-0 transition-all duration-300">
               <h1 className="text-xl lg:text-2xl font-semibold capitalize">
                 {destination.name}
